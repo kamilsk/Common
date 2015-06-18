01b7ac23b22dd737359ff92598ecc918ba27d823
Kamil Samigullin <kamil@samigullin.info>
2015-06-18 19:09:03 +0300

    add doctrine IntegerMapType

---
+++ src/Doctrine/Type/IntegerMapType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OctoLab\Common\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * @author Kamil Samigullin <kamil@samigullin.info>
+ */
+abstract class IntegerMapType extends Type
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'integer_map_type';
+    }
+
+    /**
+     * @return array
+     */
+    public function getValues()
+    {
+        return [];
+    }
+
+    /**
+     * @param array $fieldDeclaration
+     * @param AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getSmallIntTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * @param mixed $value
+     * @param AbstractPlatform $platform
+     *
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (!isset($this->getValues()[$value])) {
+            throw new \InvalidArgumentException('Invalid type.');
+        }
+        return $value;
+    }
+}
