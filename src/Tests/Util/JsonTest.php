d99a1379697b5ebd74de17c83a0cb9d30702ed57
Kamil Samigullin <kamil@samigullin.info>
2015-11-03 01:00:57 +0300

    fix issue #3

---
+++ src/Tests/Util/JsonTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace OctoLab\Common\Tests\Util;
+
+use OctoLab\Common\Util\Json;
+
+/**
+ * phpunit src/Tests/Util/JsonTest.php
+ *
+ * @author Kamil Samigullin <kamil@samigullin.info>
+ */
+class JsonTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @dataProvider jsonProvider
+     *
+     * @param Json $json
+     */
+    public function encode(Json $json)
+    {
+        $value = [];
+        self::assertJsonStringEqualsJsonString(json_encode($value), $json->encode($value));
+    }
+
+    /**
+     * @test
+     * @dataProvider jsonProvider
+     *
+     * @param Json $json
+     */
+    public function decode(Json $json)
+    {
+        $value = json_encode([]);
+        self::assertEquals(json_decode($value), $json->decode($value));
+    }
+
+    /**
+     * @test
+     * @dataProvider jsonProvider
+     * @expectedException \InvalidArgumentException
+     *
+     * @param Json $json
+     */
+    public function handleInvalidArgumentException(Json $json)
+    {
+        $value = "\xB1\x31";
+        $json->encode($value);
+    }
+
+    /**
+     * @test
+     * @dataProvider jsonProvider
+     * @expectedException \OverflowException
+     *
+     * @param Json $json
+     */
+    public function handleOverflowException(Json $json)
+    {
+        $value = '{"a":{"b":{"c":false}}}';
+        $json->decode($value, 0, 2);
+    }
+
+    /**
+     * @test
+     * @dataProvider jsonProvider
+     * @expectedException \UnexpectedValueException
+     *
+     * @param Json $json
+     */
+    public function handleUnexpectedValueException(Json $json)
+    {
+        $reflection = new \ReflectionObject($json);
+        $method = $reflection->getMethod('getException');
+        $method->setAccessible(true);
+        $json->encode([]);
+        throw $method->invoke($json);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function jsonProvider()
+    {
+        return [
+            [new Json()],
+        ];
+    }
+}
---
+++ src/Util/Json.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace OctoLab\Common\Util;
+
+/**
+ * @author Kamil Samigullin <kamil@samigullin.info>
+ */
+class Json
+{
+    /** @var bool */
+    private $assoc;
+    /** @var int */
+    private $options;
+    /** @var int */
+    private $depth;
+
+    /**
+     * @param bool $assoc
+     * @param int $options
+     * @param int $depth
+     *
+     * @api
+     */
+    public function __construct($assoc = false, $options = 0, $depth = 512)
+    {
+        $this->assoc = $assoc;
+        $this->options = $options;
+        $this->depth = $depth;
+    }
+
+    /**
+     * @param mixed $value
+     * @param int $options
+     * @param int $depth
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException when
+     *  JSON_ERROR_STATE_MISMATCH
+     *  JSON_ERROR_CTRL_CHAR
+     *  JSON_ERROR_SYNTAX
+     *  JSON_ERROR_UTF8
+     *  JSON_ERROR_INF_OR_NAN
+     *  JSON_ERROR_UNSUPPORTED_TYPE
+     * @throws \OverflowException when
+     *  JSON_ERROR_DEPTH
+     *  JSON_ERROR_RECURSION
+     * @throws \UnexpectedValueException otherwise
+     *
+     * @api
+     */
+    public function encode($value, $options = 0, $depth = 512)
+    {
+        $json = json_encode($value, $options, $depth);
+        $error = json_last_error();
+        if ($error) {
+            throw $this->getException();
+        }
+        return $json;
+    }
+
+    /**
+     * @param string $json
+     * @param bool $assoc
+     * @param int $depth
+     * @param int $options
+     *
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException when
+     *  JSON_ERROR_STATE_MISMATCH
+     *  JSON_ERROR_CTRL_CHAR
+     *  JSON_ERROR_SYNTAX
+     *  JSON_ERROR_UTF8
+     *  JSON_ERROR_INF_OR_NAN
+     *  JSON_ERROR_UNSUPPORTED_TYPE
+     * @throws \OverflowException when
+     *  JSON_ERROR_DEPTH
+     *  JSON_ERROR_RECURSION
+     * @throws \UnexpectedValueException otherwise
+     *
+     * @api
+     */
+    public function decode($json, $assoc = false, $depth = 512, $options = 0)
+    {
+        $result = json_decode($json, $assoc, $depth, $options);
+        $error = json_last_error();
+        if ($error) {
+            throw $this->getException();
+        }
+        return $result;
+    }
+
+    /**
+     * @return \InvalidArgumentException|\OverflowException|\UnexpectedValueException
+     */
+    private function getException()
+    {
+        $message = json_last_error_msg();
+        switch (json_last_error()) {
+            case JSON_ERROR_DEPTH:
+            case JSON_ERROR_RECURSION:
+                return new \OverflowException($message);
+            case JSON_ERROR_STATE_MISMATCH:
+            case JSON_ERROR_CTRL_CHAR:
+            case JSON_ERROR_SYNTAX:
+            case JSON_ERROR_UTF8:
+            case JSON_ERROR_INF_OR_NAN:
+            case JSON_ERROR_UNSUPPORTED_TYPE:
+                return new \InvalidArgumentException($message);
+            default:
+                return new \UnexpectedValueException($message);
+        }
+    }
+}
